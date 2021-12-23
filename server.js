56f6f4c2fbc4a01d6c06bd0ef556d2ffd4866a3b
rytc <rytc@protonmail.com>
2021-12-23 13:31:45 -0800

    Compressed game data

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { join } = require('path')
-const sudoku = require('sudoku-gen')
+const sudoku = require('./lib/sudoku.js')
 
 const app = express()
 
@@ -18,21 +18,28 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/sudoku/new/:difficulty', (req, res) => {
-  let puzzle = sudoku.getSudoku(req.params.difficulty);
+  let puzzle = sudoku.createNewPuzzle(req.params.difficulty);
   // Insert into db
   res.json(puzzle);
 })
 
 app.get('/api/sudoku/:id', (req, res) => {
   // Fetch puzzle from mysql by req.param.db
+  // let puzzle = sudoku.createFromDB(game.gameData)
   let puzzle = {}
-  // Insert into db
   res.json(puzzle);
 })
 
-app.put('/api/sudoku/:cellIndex/:number', (req, res) => {
-  // Insert req.params.number into req.params.cellIndex
-  // Insert into db
+app.put('/api/sudoku/:id/:cellIndex/:number', (req, res) => {
+  /*
+  // fetch from db using req.param.id
+  let puzzle = sudoku.createFromDB(game.gameData)
+  if(puzzle.given[req.params.cellIndex] === 0) {
+    puzzle.board[req.params.cellIndex] = req.params.number
+  }
+  // let gameData = puzzle.compress()
+  // update game table by req.param.id set gameData to ${gameData}
+  */
   res.sendStatus(200);
 })
 
@@ -41,8 +48,17 @@ app.get('/game', (req, res) => {
 })
 
 
-
-
-
+/*
+// For testing the compression
+let compressTest = () => {
+  let newPuzzle  = sudoku.createNewPuzzle("easy");
+  console.log(newPuzzle);
+  let data = newPuzzle.compress();
+  console.log(data.length);
+  let decomp = sudoku.createFromDB(data);
+  console.log(decomp);
+}
+compressTest()
+*/
 
 app.listen(3000)
