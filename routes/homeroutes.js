5fdae5a18fadd7db43b09f34281a923647615efe
bradleyjosephh <bradleyjosephh@gmail.com>
2022-01-04 13:50:54 -0800

    fixed overlapping issues

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -23,47 +23,12 @@ app.get('/difficulty', (req, res) => {
 })
 
 
-<<<<<<< HEAD
 app.get('/blog', helpers.isLoggedIn, async (req, res) => {
   let viewData = {
       isLoggedIn: req.session.loggedIn ? true : false,
       username: req.session.loggedIn ? req.session.username : "ERROR"
   }
   res.render('blog', viewData)
-=======
-app.get('/blog', (req, res) => {
-  res.render('blog')
-})
-
-app.get('/api/sudoku/new/:difficulty', (req, res) => {
-  let puzzle = sudoku.createNewPuzzle(req.params.difficulty);
-  // Insert into db
-  res.json(puzzle);
-})
-
-app.get('/api/sudoku/:id', async (req, res) => {
-  // Fetch puzzle from mysql by req.param.db
-  /*
-    let game = await Game.fetchAll({where: {id: req.param.id}}).then();
-    let puzzle = sudoku.createFromDB(game.gameData);
-    res.json(puzzle)
-  */
-  let puzzle = {}
-  res.json(puzzle);
-})
-
-app.put('/api/sudoku/:id/:cellIndex/:number', async (req, res) => {
-  /*
-  let game = await Game.fetchAll({where: {id: req.param.id}}) 
-  let puzzle = sudoku.createFromDB(game.gameData)
-  if(puzzle.given[req.params.cellIndex] === 0) {
-    puzzle.board[req.params.cellIndex] = req.params.number
-  }
-  let gameData = puzzle.compress()
-  Game.update({gameData: puzzle.compress}, {where: {id: req.params.id}}) 
-  */
-  res.sendStatus(200);
->>>>>>> 348bf2081a9bc6c91e6e33fd930b51f35d904ffd
 })
 
 
