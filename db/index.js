185bbff6b2ad5ac48edfd80ca02df7858c7e6f9b
email <abe.solis2015@gmail.com>
2022-01-05 15:49:27 -0800

    Heroku DB link

--- db/index.js
+++ db/index.js
@@ -1,3 +1,4 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize('mysql://root:rootroot@localhost:3306/sudoku_db')
+
+module.exports = new Sequelize(process.env.JAWSDB || 'mysql://root:rootroot@localhost:3306/sudoku_db')
