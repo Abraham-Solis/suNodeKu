3bdc72646184e6166ca1315826e4ca7ad48f85cb
email <abe.solis2015@gmail.com>
2022-01-05 16:05:43 -0800

    jawsdb url

--- db/index.js
+++ db/index.js
@@ -1,3 +1,3 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.JAWSDB || 'mysql://root:rootroot@localhost:3306/sudoku_db')
+module.exports = new Sequelize(process.env.JAWSDB_URL || 'mysql://root:rootroot@localhost:3306/sudoku_db')
